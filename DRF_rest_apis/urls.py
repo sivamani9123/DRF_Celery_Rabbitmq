02123e9955cc2a7513133f79ca220b5c80021046
sivamani9123 <72572309+sivamani9123@users.noreply.github.com>
2025-06-11 00:40:18 -0400

    Update urls.py

--- DRF_rest_apis/urls.py
+++ DRF_rest_apis/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('myapp.urls')),
+    path('admin/', admin.site.urls), //Kind of login page
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), // provide access token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), // To get refresh token 
+    path('api/', include('myapp.urls')), 
 ]
